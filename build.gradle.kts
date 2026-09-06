d2b136c6cc41ae65e87c518b358e99fc3ac8617a
Md Sabbir <103810163+thenamesabbir@users.noreply.github.com>
2026-09-06 22:55:47 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         maven("https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-        classpath("com.github.recloudstream:gradle:32895aedb6-1")
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
     }
 }
 
